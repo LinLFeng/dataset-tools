332d4a947be6e4d5bb45e9fd2ceb6e5e51ed12ec
佰碎叁 <LIN_LFeng@outlook.com>
2024-09-28 21:50:06 +0800

    修改进度显示实现，控制刷新时间

--- utils/progress.py
+++ utils/progress.py
@@ -1,20 +1,33 @@
+import time
+
+
 class Progress:
-    def __init__(self, data, text="progress") -> None:
+    def __init__(self, data, text="progress"):
+        # 可迭代数据
         self.data = data
+        # 索引
         self.data_idx = 0
-        self.data_len = len(data)
+        # 进度标题
         self.text = text
+        # 进度更新时间
+        self.last_update = time.time()
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.data_idx < self.data_len:
-            ask = self.data[self.data_idx]
+        if self.data_idx < len(self.data):
+            item = self.data[self.data_idx]
             self.data_idx += 1
-            progress = (self.data_idx / self.data_len) * 100
-            print(f"\r%s - %d%%" % (self.text, progress), end="")
-            return ask
+            now = time.time()
+            if now - self.last_update > 0.3:
+                # 更新时间
+                self.last_update = now
+                # 计算比例
+                progress = (self.data_idx / len(self.data)) * 100
+                print(f"\r{self.text} - {progress:.2f}%", end="", flush=True)
+            return item
         else:
-            print("")
+            # 结束后确保是 100%
+            print(f"\r{self.text} - 100.00%", end="\n", flush=True)
             raise StopIteration()
