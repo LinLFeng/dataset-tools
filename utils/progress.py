e5f19d12da002ece2a0cd8239989eeec99fcae96
佰碎叁 <LIN_LFeng@outlook.com>
2024-10-05 14:52:14 +0800

    本地进度条实现细节修改，显示样式修改

--- utils/progress.py
+++ utils/progress.py
@@ -4,30 +4,31 @@ import time
 class Progress:
     def __init__(self, data, text="progress"):
         # 可迭代数据
-        self.data = data
+        self._data = data
         # 索引
-        self.data_idx = 0
+        self._data_idx = 0
         # 进度标题
-        self.text = text
+        self._text = text
         # 进度更新时间
-        self.last_update = time.time()
+        self._last_update = time.time()
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.data_idx < len(self.data):
-            item = self.data[self.data_idx]
-            self.data_idx += 1
+        if self._data_idx < len(self._data):
+            item = self._data[self._data_idx]
+            self._data_idx += 1
             now = time.time()
-            if now - self.last_update > 0.3:
+            if now - self._last_update > 0.3:
                 # 更新时间
-                self.last_update = now
+                self._last_update = now
                 # 计算比例
-                progress = (self.data_idx / len(self.data)) * 100
-                print(f"\r{self.text} - {progress:.2f}%", end="", flush=True)
+                statistics = f"[{self._text} - [done {self._data_idx}] / [total {len(self._data)}]]"
+                print(f"\r{statistics}", end="", flush=True)
             return item
         else:
             # 结束后确保是 100%
-            print(f"\r{self.text} - 100.00%", end="\n", flush=True)
+            statistics = f"[{self._text} - [done {len(self._data)}] / [total {len(self._data)}]] Done"
+            print(f"\r{statistics}", end="\n", flush=True)
             raise StopIteration()
