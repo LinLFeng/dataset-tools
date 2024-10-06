1b056853cd443692feeed3072ce654894c2a1461
佰碎叁 <LIN_LFeng@outlook.com>
2024-10-06 13:11:23 +0800

    进度显示格式调整

--- utils/progress.py
+++ utils/progress.py
@@ -3,13 +3,15 @@ from utils.log import normal_log, process_log
 
 
 class Progress:
-    def __init__(self, data, text="progress"):
+    def __init__(self, data, model="progress", title="progress"):
         # 可迭代数据
         self._data = data
         # 索引
         self._data_idx = 0
+        # 所在模块
+        self._model = model
         # 进度标题
-        self._text = text
+        self._title = title
         # 进度更新时间
         self._last_update = time.time()
 
@@ -24,11 +26,18 @@ class Progress:
             if now - self._last_update > 0.3:
                 # 更新时间
                 self._last_update = now
-                statistics = f"[done {self._data_idx}] / [total {len(self._data)}]"
-                process_log(self._text, statistics, end="", flush=True)
+                statistics = f"{self._title} [done {self._data_idx}] / [total {len(self._data)}]"
+                process_log(
+                    module=self._model,
+                    msg=statistics,
+                    end="",
+                    flush=True)
             return item
         else:
             # 结束后确保是 100%
-            statistics = f"[done {len(self._data)}] / [total {len(self._data)}]"
-            process_log(self._text, statistics, flush=True)
+            statistics = f"{self._title} [done {len(self._data)}] / [total {len(self._data)}]"
+            process_log(
+                module=self._model,
+                msg=statistics,
+                flush=True)
             raise StopIteration()
