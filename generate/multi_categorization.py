d6253011a3191cfcf59ea6cc411ce818e62e8cb0
佰碎叁 <LIN_LFeng@outlook.com>
2024-10-06 14:45:26 +0800

    多分类数据生成list.txt功能重写

---
+++ generate/multi_categorization.py
@@ -0,0 +1,64 @@
+import os
+from utils.loader import BaseLoader
+from utils.progress import Progress
+from utils.log import normal_log, process_log
+
+
+def multi_categorization(
+        path,
+        train_prop=0.8,
+        val_prop=0.2,
+        cut_prop=0.01,
+        folder_code=None,
+        shuffle=False,
+):
+    process_log("multi_categorization", "build start")
+    folders_loader = BaseLoader()
+    folders_loader.load(path, folder_code=folder_code)
+    if shuffle:
+        folders_loader.shuffle()
+    if cut_prop != 0:
+        folders_loader.cut(cut_prop)
+    folders_loader.split(train_prop=train_prop, val_prop=val_prop)
+
+    # TODO: 原始数据集精简功能
+    image = folders_loader.get_data()
+
+    for file_name in ["train", "val", "test"]:
+        with open(os.path.join(path, f"{file_name}_list.txt"), "a") as file:
+            for j in Progress(image[file_name],
+                              model="multi_categorization",
+                              title=f"{file_name}_list.txt building"):
+                file.write(f"{j}\n")
+    process_log("multi_categorization", "build complete")
+
+
+if __name__ == "__main__":
+    # 分类名称到编号的映射
+    class_dict = {
+        "cardboard": "584",
+        "glass": "491",
+        "metal": "400",
+        "paper": "472",
+        "plastic": "127",
+        "trash": "393",
+    }
+    # 原始数据集根路径(纯英文)
+    dir1 = "D:\\Datasets\\Garbage classification"
+    # 设置训练\验证集比例
+    # 若二值相加为1,则不划分验证集,反之,比例为1-train_p-val_p
+    train_p = 0.8
+    val_p = 0.2
+    # 数据选择比例
+    cut_p = 1
+    # 数据乱序
+    shu = True
+
+    multi_categorization(
+        path=dir1,
+        train_prop=train_p,
+        val_prop=val_p,
+        cut_prop=cut_p,
+        folder_code=class_dict,
+        shuffle=shu,
+    )
