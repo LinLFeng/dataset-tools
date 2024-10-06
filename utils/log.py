9a230273987da60a2f0f4282e3330a793d13dd91
佰碎叁 <LIN_LFeng@outlook.com>
2024-10-06 22:12:45 +0800

    统一日志功能实现重写

--- utils/log.py
+++ utils/log.py
@@ -1,36 +1,28 @@
 import time
 
+from pyexpat.errors import messages
 
-class Bcolors:
-    NORMAL = "\033[92m"  # GREEN
-    PROCESS = "\033[94m"  # BLUE
-    WARNING = "\033[93m"  # YELLOW
-    ERROR = "\033[91m"  # RED
-    RESET = "\033[0m"  # RESET COLOR
-
-
-def get_time() -> str:
-    localtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    return localtime
+INFO = "\033[92m"  # GREEN
+MAIN = "\033[94m"  # BLUE
+WARN = "\033[93m"  # YELLOW
+ERROR = "\033[91m"  # RED
+RESET = "\033[0m"  # RESET COLOR
 
 
-def normal_log(module, msg):
-    message = f"{get_time()} [INFO]    [{module}] {msg}"
-    print(Bcolors.NORMAL + message + Bcolors.RESET)
-
-
-def process_log(module, msg, flush=False, end="\n"):
-    message = f"{get_time()} [PROCESS] [{module}] {msg}"
+def log(module, msg, flush=False, end="\n", level=0):
+    message = ""
     if flush:
-        message = "\r" + message
-    print(Bcolors.PROCESS + message + Bcolors.RESET, flush=flush, end=end)
-
-
-def warning_log(module, msg):
-    message = f"{get_time()} [WARNING] [{module}] {msg}"
-    print(Bcolors.WARNING + message + Bcolors.RESET)
-
-
-def error_log(module, msg):
-    message = f"{get_time()} [ERROR]   [{module}] {msg}"
-    print(Bcolors.ERROR + message + Bcolors.RESET)
+        message += "\r"
+    message += time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    if level == 0:
+        message += INFO + " [INFO] "
+    elif level == 1:
+        message += MAIN + " [MAIN] "
+    elif level == 2:
+        message += WARN + " [WARN] "
+    elif level == 3:
+        message += ERROR + " [ERROR]"
+    message += f" [{module}] "
+    message += msg
+    message += RESET
+    print(message, end=end, flush=flush)
